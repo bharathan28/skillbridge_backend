294183d642310e33899e71cf39f6a5d0a9e529b3
bharathan28 <bS.Obhuvanesh01@gmail.com>
2026-05-06 20:16:06 +0530

    err-1

--- skillbridge_backend/config/settings.py
+++ skillbridge_backend/config/settings.py
@@ -23,8 +23,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
-
+CORS_ALLOWED_ORIGINS = [
+    "https://skillbridge-frontend-fawn.vercel.app",
+]
 
 CORS_ALLOW_HEADERS = [
     "content-type",
